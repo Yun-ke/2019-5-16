bb35ba3931992c607c7eff675c496b7d1ea19929
miyunke <miyunke@iauto.com>
2019-05-16 13:52:42 +0800

    add test

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ int main()
     int system;
     std::cout << "Please input a number: ";
     std::cin >> system;
+    system += 3;
     std::cout << "The system is: " << system << std::endl;
     return 0;
 }
