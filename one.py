0899e3adc7fc3c5c201422147845fee62f2cf615
miyunke <miyunke@iauto.com>
2019-05-16 14:48:17 +0800

    add one.py

---
+++ one.py
@@ -0,0 +1,10 @@
+def print_lyrics():
+    print("My name is miyunke")
+    print("Life is so boring")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+print_lyrics()
+repeat_lyrics()
